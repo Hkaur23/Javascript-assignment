28937ed9da8f9b344f422f96e0d0eb850f2c3bc9
Harman Kaur <mharman20@yahoo.in>
2022-10-18 11:01:45 -0600

    Completed javascript assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+let itemPrice = 50;
+let walletMoney = 35;
+let totalFriends = 60;
+let myAge = 30;
+let firstName = "Harman";
+let lastName = "Kaur";
+let middleName = "Mann";
+
+/* New amount of money in wallet after buying the item is */
+
+console.log("Item price is: $"+itemPrice);
+console.log("Total money in wallet is: $"+walletMoney);
+
+console.log("Total money left in wallet after purchase is: $"+(itemPrice - walletMoney));
+
+/* Number of friends made each year based on age and number of friends variable */
+
+console.log("Total numbers of friends are: ", totalFriends);
+console.log("My Total age is : ", myAge);
+console.log("Friends made each year is : ", totalFriends / myAge);
+
+/*Full name based on first name, middle initial, and last name */
+
+console.log("Full Name  : ", firstName +' ' + middleName + ' ' + lastName);
